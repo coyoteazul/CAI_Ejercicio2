02e911ac2c00d4df86c3148321889ced84274ffe
coyoteazul <hernanruiz91@gmail.com>
2021-03-15 23:48:53 -0300

    Initial commit

---
+++ CAI_Ejercicio2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CAI_Ejercicio2
+{
+    class Program
+    {
+        static bool IsPrimo (int num)
+        {
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 805; i++)
+            {
+                string suffix = null;
+                if (IsPrimo(i))
+                {
+                    suffix += " Es primo";
+                }
+                if (i % 3 == 0)
+                {
+                    if (!string.IsNullOrEmpty(suffix))
+                        suffix += " y";
+                    suffix += " Es divisible por 3";
+                }
+
+                if (!string.IsNullOrEmpty(suffix))
+                {
+                    Console.WriteLine(i + suffix);
+                }
+            }
+        }
+    }
+}
